de264f4003d4c9f5fa827dc3f70280fab7636b6c
ReyRizki <reynaldirizki97.rr@gmail.com>
2020-10-06 21:51:47 +0700

    add file reading

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from bst import BST
 
+def fileToTree(tree, file_name):
+    file1 = open(file_name, 'r') 
+    while True:
+        # Get next line from file 
+        line = file1.readline() 
+    
+        # if line is empty 
+        # end of file is reached 
+        if not line: 
+            break
+        
+        line = line.split(' ')
+
+        for word in line:
+            tree.insert(word, file_name)
+    
+    file1.close()
+
 tree = BST()
 
-tree.insert("Rey", "UwU")
-tree.insert("Ganteng", "UwU")
-tree.insert("Rey", "LOL")
-tree.insert("Rey", "UwU")
+fileToTree(tree, 'test_file.txt')
 
-print(tree.inorder())
-
-print(tree.search("Rey"))
-print(tree.search("Jelek"))
+print(tree.search('BTS'))
